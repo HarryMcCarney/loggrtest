4e0033d1c67b703284738d7d17cedcdd843caf9b
Harry McCarney <hmccarney@gmail.com>
2013-08-03 19:38:42 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Loggr;
+using NLog;
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        public static readonly Logger log = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            Events.Create().Text("helloworld").Post();
+
+            log.Error("ERROR WORLD");
+        }
+    }
+}
